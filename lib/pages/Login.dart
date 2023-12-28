bf8dc8949aae431cfaa9b92434f6ad1d52cba985
showravdas11 <showravdas11@gmail.com>
2023-12-28 19:02:17 +0600

    day 1-9 finished

--- lib/pages/Login.dart
+++ lib/pages/Login.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:thirty_days_flutter/pages/home_page.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -8,54 +9,133 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  String name = "";
+  bool changeBtn = false;
+
+  final _formKey = GlobalKey<FormState>();
+
+  moveToHome() async {
+
+    if (_formKey.currentState!.validate()) {
+      setState(() {
+      changeBtn = true;
+    });
+
+    await Future.delayed(Duration(seconds: 1));
+
+    await Navigator.push(
+        context, MaterialPageRoute(builder: (context) => HomePage()));
+
+    setState(() {
+      changeBtn = false;
+    });
+    }
+
+
+    
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Column(
-        children: [
-          Image.asset(
-            "assets/images/Login30.png",
-            fit: BoxFit.cover,
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          Text(
-            "Welcome",
-            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 32.0, vertical: 16.0),
-            child: Column(
-              children: [
-                TextFormField(
-                  decoration: InputDecoration(
-                      hintText: "Enter User Name", labelText: "UserName"),
+      body: SingleChildScrollView(
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              Image.asset(
+                "assets/images/Login30.png",
+                fit: BoxFit.cover,
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Text(
+                "Welcome $name",
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 32.0, vertical: 16.0),
+                child: Column(
+                  children: [
+                    TextFormField(
+                      decoration: InputDecoration(
+                          hintText: "Enter User Name", labelText: "UserName"),
+                      onChanged: (value) {
+                        setState(() {
+                          name = value;
+                        });
+                      },
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return "User name can't be empty";
+                        }
+                        return null;
+                      },
+                    ),
+                    TextFormField(
+                      obscureText: true,
+                      decoration: InputDecoration(
+                          hintText: "Enter Password", labelText: "Password"),
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return "Password can't be empty";
+                        }else if (value.length <6) {
+                          return "Password length should be atleast 6";
+                        }
+                        return null;
+                      },
+                    ),
+                    SizedBox(
+                      height: 40,
+                    ),
+
+                    InkWell(
+                      onTap: () => moveToHome(),
+                      child: AnimatedContainer(
+                        duration: Duration(seconds: 1),
+                        width: changeBtn ? 50 : 150,
+                        height: 50,
+                        alignment: Alignment.center,
+                        child: changeBtn
+                            ? Icon(
+                                Icons.done,
+                                color: Colors.white,
+                              )
+                            : Text(
+                                "Login",
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 18),
+                              ),
+                        decoration: BoxDecoration(
+                            color: Colors.deepPurple,
+                            borderRadius:
+                                BorderRadius.circular(changeBtn ? 50 : 8)),
+                      ),
+                    )
+
+                    // ElevatedButton(
+                    //   style: ElevatedButton.styleFrom(
+                    //     backgroundColor: Colors.deepPurple,
+                    //     foregroundColor: Colors.white,
+                    //     minimumSize: Size(600, 45)
+                    //   ),
+                    //   onPressed: () {
+                    //     Navigator.push(context, MaterialPageRoute(builder: (context) => HomePage()));
+                    //   },
+                    //   child: Text("Login"))
+                  ],
                 ),
-                TextFormField(
-                  obscureText: true,
-                  decoration: InputDecoration(
-                      hintText: "Enter Password", labelText: "Password"),
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.deepPurple,
-                    foregroundColor: Colors.white,
-                    minimumSize: Size(600, 45)
-                  ),
-                  onPressed: () {},
-                  child: Text("Login"))
-              ],
-            ),
-          )
-        ],
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -11,7 +11,9 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      appBar: AppBar(
+        title: Text("HOME PAGE"),
+      ),
     );
   }
 }
